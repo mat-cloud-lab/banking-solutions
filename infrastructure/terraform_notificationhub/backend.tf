716bd7f6af8f8361c042cd0dee6262a98d8614cf
Mateusz Urbański <matiszak@gmail.com>
2023-06-11 17:26:45 +0200

    Add placeholder for azure devops

--- infrastructure/terraform_notificationhub/backend.tf
+++ infrastructure/terraform_notificationhub/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "notificationhubtfstate"
-    resource_group_name = "rg-we-01-notification-hub-setup"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
+    storage_account_name = "#{tfStateAzureStorageAccountName}#"
+    resource_group_name = "#{tfStateResourceGroupName}#"
+    container_name = "#{tfStateContainerName}#"
+    key = "#{tfStateFileName}#"
   }
 }
